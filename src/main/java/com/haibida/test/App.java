2f09ec3dc53f065689e1a11c4acddcc1beba256e
liwenping <1901712201@qq.com>
2020-08-30 13:29:24 +0800

    提交到本地库

---
+++ src/main/java/com/haibida/test/App.java
@@ -0,0 +1,14 @@
+package com.haibida.test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Create Git Repository-----初始化为本地库-----字体变红色");
+        System.out.println("add------添加到暂存区------字体变绿色");
+        System.out.println("");
+
+    }
+}
